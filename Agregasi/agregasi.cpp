02a28532792170d926e0743156faa2ca1b6af44f
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-27 10:47:30 +0700

    membuat objek 3 anak (tono, rini, dewi)

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -8,4 +8,8 @@ using namespace std;
 int main(){
     ibu* varIbu = new ibu("dini");
     ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak ("tono");
+    anak* varAnak2 = new anak ("rini");
+    anak* varAnak3 = new anak ("dewi");
+    
 }
